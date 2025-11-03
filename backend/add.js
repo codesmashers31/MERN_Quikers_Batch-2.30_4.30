cf60d072331e9dbaa2b17e6bfb2760e091b9670a
Bala <balasudahn17@gmail.com>
2025-11-03 16:27:42 +0530

    updated

---
+++ backend/add.js
@@ -0,0 +1,23 @@
+const add = (a,b)=>{
+
+return a+b
+
+}
+
+export const sub = (a,b)=>{
+
+return a-b
+
+}
+
+export default add;
+
+
+
+const leo = (charA,CharB,CharC)=>{
+
+console.log(charA);
+
+
+
+}
---
+++ backend/main.js
@@ -0,0 +1,6 @@
+import add,{sub} from './add.js'
+
+console.log(add(5,2));
+
+
+console.log(sub(8,7));
